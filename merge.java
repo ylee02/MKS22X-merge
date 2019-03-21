e57af4ffd00f48519f12b62ee5f72b2ab3bf67e6
ylee02 <ylee01@stuy.edu>
2019-03-21 12:08:11 +0900

    Added mergesort skeleton

--- merge.java
+++ merge.java
@@ -1,3 +1,16 @@
 public class merge {
+  public static void mergesort(int[] data) {
+	  mergesort(data, 0, data.length - 1);
+
+  }
   
+  public static void mergesort(intp[ data, int lo, int hi) {
+	  if (lo >= hi) {
+		  return;
+	  }
+	  int pivot = (high+low) / 2;
+	  mergesort(data, lo, pivot);
+	  mergesort(data, pivot + 1, hi);
+	  merge(data, lo, pivot + 1, hi);
+  }
 }
